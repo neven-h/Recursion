ceb6e2bb6a50e2010f7a6e35cc944033bd3248ae
neven-h <69196529+neven-h@users.noreply.github.com>
2020-08-24 10:36:54 +0300

    Add files via upload

---
+++ Ways.java
@@ -0,0 +1,18 @@
+public class Ways {
+
+
+    public static int ways(int k, int n) {
+        return ways(k, n, 0);
+    }
+
+    public static int ways(int k, int n, int location) {
+        if (k == 0) {
+            return 1;
+        }
+        //original code as appears in the answers includes the next 3 rows:
+//        else {
+//            return 0;
+//        }
+        return ways(k - 1, n, location + 1) + ways(k - 1, n, location - 1);
+    }
+}
