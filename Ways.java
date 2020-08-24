4123c72cc45e6c9889dbfeb901e6e06b5ed22b2a
neven-h <69196529+neven-h@users.noreply.github.com>
2020-08-24 10:44:30 +0300

    Update Ways.java

--- Ways.java
+++ Ways.java
@@ -1,19 +1,14 @@
 public class Ways {
 
-
-    public static int ways(int k, int n) {
-        return ways(k, n, 0);
-    }
-
-    public static int ways(int k, int n, int location) {
+      public static int ways(int k, int n, int location) {
         if (k == 0) {
-            return 1;
-        
-        //original code as appears in the answers includes the next 3 rows:
-        else {
-           return 0;
-       }
+            if (location == n) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
         return ways(k - 1, n, location + 1) + ways(k - 1, n, location - 1);
     }
+
 }
